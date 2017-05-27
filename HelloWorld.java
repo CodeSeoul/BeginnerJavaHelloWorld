780deda7f76cf721b7c60e8e33b39f136ded0cb5
Bryan "Beege" Berry <bryanberry235711@gmail.com>
2017-05-27 18:23:41 +0900

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+// this is a single line comment
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.err.println("Hello world!");
+	}
+}
+/*
+
+this is a multiline comment
+Anything between the slash star and star slash
+will be ignored by Java
+Use these to take notes.
+
+*/
